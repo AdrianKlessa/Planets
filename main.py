75fe5f0590b737e1765551de0965f634f0eee740
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2022-11-13 00:40:32 +0100

    Added basic spaceship

--- main.py
+++ main.py
@@ -15,48 +15,91 @@ EARTH_DISTANCE_FROM_SUN = 148.1*(10**9)
 EARTH_VELOCITY = 29.72*10**3  # m/s
 
 EARTH_POS = np.array(normalize_vector(np.array([1,1])),dtype=float)*EARTH_DISTANCE_FROM_SUN
-
+SPACESHIP_POS = np.array(normalize_vector(np.array([2,1])),dtype=float)*EARTH_DISTANCE_FROM_SUN
 
 # TODO: Fix the positions so that sun is actually in the middle of the screen
 # TODO: Add more planets, change their render size
-# TODO: Add spaceship
-# TODO: Add spaceship controls
+
+
+#Planetary bodies
 Sun = phys.MyPhysObject(DISPLAY_WIDTH//2, DISPLAY_HEIGHT//2, 1.9891*(10**30), 0, 0)
 Earth = phys.MyPhysObject(EARTH_POS[0],EARTH_POS[1], 5.972*(10**24), 0, 0)
 Earth.set_velocity(np.array([0,-1], dtype=float), EARTH_VELOCITY)
+
+#The spaceship
+#Spaceship = phys.Spaceship(900,900,96570,0,0,92670,340,2960,0,np.array([1, 0], dtype=float))
+Spaceship = phys.Spaceship(SPACESHIP_POS[0],SPACESHIP_POS[1],96570,0,0,92670,340*1000,2960,0,np.array([0, 1], dtype=float))
 
 
 Simulation = phys.Simulation()
 Simulation.list_of_objects["Earth"] = Earth
 Simulation.list_of_objects["Sun"] = Sun
+Simulation.list_of_objects["Spaceship"] = Spaceship
 
 display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 display.fill((0, 0, 0))
 
 space_factor = 150*(10**7) #Since SI unit of distance is m we would have planets really far away in space, not visible on screen
 
+def sim_position_to_screen_position(body_pos_vector):
+    return (body_pos_vector[0]//space_factor+DISPLAY_WIDTH//3, body_pos_vector[1]//space_factor+DISPLAY_HEIGHT//3)
 
 def draw_body(display, body, color, r):
+    screen_pos = sim_position_to_screen_position(body.position)
     pygame.draw.circle(display, color,
-                       center=(body.position[0]//space_factor+DISPLAY_WIDTH//3, body.position[1]//space_factor+DISPLAY_HEIGHT//3), radius=r, width=0)
+                       center=(screen_pos[0], screen_pos[1]), radius=r, width=0)
     #print(str(body.position[0]//space_factor+DISPLAY_WIDTH//2)+", "+str(body.position[1]//space_factor+DISPLAY_HEIGHT//2))
 
-Simulation.multiplier=365*24
+def draw_spaceship(display, ship):
+    pos = ship.position
+    SPACESHIP_SIZE = 5*space_factor
+    #Draw a triangle representing the ship, a "spear point" pointing in the ship's current orientation
+    normalized = normalize_vector(ship.direction)
+    point_1 = sim_position_to_screen_position((normalized*3*SPACESHIP_SIZE)+pos) #The tip
+    point_2 = sim_position_to_screen_position(np.array([normalized[1],-1*normalized[0]], dtype=float)*SPACESHIP_SIZE+pos)
+    point_3 = sim_position_to_screen_position(np.array([-1*normalized[1], normalized[0]], dtype=float)*SPACESHIP_SIZE+pos)
+
+    pygame.draw.line(display, (255, 0, 0),
+                     point_1, point_2, width=2)
+    pygame.draw.line(display, (255, 0, 0),
+                     point_1, point_3, width=2)
+    pygame.draw.line(display, (255, 255, 255),
+                     point_2, point_3, width=2)
+
+
+Simulation.multiplier=365
+
+
+rotation_angle=30
 
 while True:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                Spaceship.rotate_anticlockwise(rotation_angle)
+                print("Rotating")
+            if event.key == pygame.K_RIGHT:
+                Spaceship.rotate_anticlockwise(360-rotation_angle)
+                print("Rotating")
+            if event.key == pygame.K_UP:
+                Spaceship.current_flow_rate=Spaceship.max_flow_rate
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
     display.fill((0, 0, 0))
     Simulation.update()
-    for key, planet in Simulation.list_of_objects.items():
+    print(Spaceship.fuel_mass)
+    for key, obj in Simulation.list_of_objects.items():
         if key == "Earth":
             #print("Drawing Earth")
-            draw_body(display,planet, pygame.Color('forestgreen'), 10)
+            draw_body(display,obj, pygame.Color('forestgreen'), 10)
             #draw_body(display, planet, (125,0,125), 50)
         if key == "Sun":
             #print("Drawing Sun")
-            draw_body(display, planet, pygame.Color('goldenrod1'), 10)
+            draw_body(display, obj, pygame.Color('goldenrod1'), 10)
             #draw_body(display, planet, (125,0,125), 100)
+        if key == "Spaceship":
+            #print("Drawing Spaceship")
+            draw_spaceship(display, obj)
     pygame.display.update()
--- phys.py
+++ phys.py
@@ -1,7 +1,7 @@
 # A planet, a spacecraft, a comet, whatever
 import scipy.constants as spc
 import numpy as np
-
+import math
 
 def normalize_vector(a):
     return (a / np.sqrt(np.sum(a ** 2)))
@@ -30,15 +30,16 @@ class MyPhysObject:
 
 # Thanks https://space.stackexchange.com/questions/30497
 class Spaceship(MyPhysObject):  # Default based on Falcon 9, hope I'm not messing up the data here
-    fuel_mass = 96670  # kg
+    fuel_mass = 92670  # kg
     specific_impulse = 340  # s
     max_flow_rate = 2960  # kg/s
     current_flow_rate = 0  # kg/s
-    direction = np.array([0, 0], dtype=float)  # Facing direction, vector, hopefully normalized but let's assume not
+    direction = np.array([1, 0], dtype=float)  # Facing direction, vector, hopefully normalized but let's assume not
 
     def calculate_thrust(self):
         return self.current_flow_rate * self.specific_impulse
 
+    # TODO: Make sure we don't go into negative fuel_mass due to large time multipliers
     def update(self, time_multiplier):
         if self.fuel_mass>0:
             force = self.calculate_thrust() * time_multiplier
@@ -46,10 +47,24 @@ class Spaceship(MyPhysObject):  # Default based on Falcon 9, hope I'm not messin
             self.mass -= fuel_used
             self.fuel_mass -= fuel_used
             normalized = normalize_vector(self.direction)
-            self.use_force(force * normalized)
+            self.use_force(force * normalized*-1)
         self.position += self.velocity * time_multiplier
 
+    def __init__(self, x, y, mass, velocity_x, velocity_y, fuel_mass, specific_impulse, max_flow_rate, current_flow_rate, direction):
+        super().__init__(x, y, mass, velocity_x, velocity_y)
+        self.fuel_mass = fuel_mass
+        self.specific_impulse = specific_impulse
+        self.max_flow_rate = max_flow_rate
+        self.current_flow_rate = current_flow_rate
+        self.direction = direction
 
+    def rotate_anticlockwise(self, deg):
+        rad = math.radians(deg)
+        new_direction = np.array([0, 0], dtype=float)
+        new_direction[0] = ((math.cos(rad)*self.direction[0])-(math.sin(rad)*self.direction[1]*-1))
+        new_direction[1] = ((math.sin(rad) * self.direction[0]) + (math.cos(rad) * self.direction[1]*-1))*-1
+        self.direction=new_direction
+        print(self.direction)
 def calculate_gravity(object1, object2):
     distance = np.linalg.norm(object1.position - object2.position)
     return (spc.G * object1.mass * object2.mass) / (distance ** 2)
