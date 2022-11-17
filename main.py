74352c3bd3101cb7bbd2c8d0641dfdabd82a62b6
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2022-11-17 19:44:01 +0100

    More comment cleanup

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import tools
 pygame.init()
 
 
-
 DISPLAY_WIDTH = 1920
 DISPLAY_HEIGHT = 1080
 
-EARTH_DISTANCE_FROM_SUN = 148.1*(10**9) #m
+EARTH_DISTANCE_FROM_SUN = 148.1*(10**9)  # m
 EARTH_VELOCITY = 29.72*10**3  # m/s
 
 EARTH_POS = np.array(tools.normalize_vector(np.array([1,1])),dtype=float)*EARTH_DISTANCE_FROM_SUN
@@ -20,7 +19,8 @@ SPACESHIP_POS = np.array(tools.normalize_vector(np.array([2,1])),dtype=float)*EA
 # TODO: Fix the positions so that sun is actually in the middle of the screen
 # TODO: Add more planets, change their render size
 # TODO: Add an AI, make the rewards between -1 and 1 to fit the tanh function
-# TODO: Measure the minimum distance from a spaceship to a particular planet (Mars?), make the reward a function of that distance after a set time.
+# TODO: Measure the minimum distance from a spaceship to a particular planet (Mars?),
+#  make the reward a function of that distance after a set time.
 # TODO: Once the spaceship is out of fuel, no need to ask the AI for anything since it can't do anything
 
 
@@ -29,28 +29,19 @@ Screen_pos = np.array([0,0])
 SCREEN_MOVE_FACTOR = 100 #  How much the screen moves with each keypress
 
 
-#Planetary bodies
-#Sun = phys.MyPhysObject(DISPLAY_WIDTH//2, DISPLAY_HEIGHT//2, 1.9891*(10**30), 0, 0)
-Sun = phys.MyPhysObject(0, 0, 1.9891*(10**30), 0, 0)
-Earth = phys.MyPhysObject(EARTH_POS[0],EARTH_POS[1], 5.972*(10**24), 0, 0)
-Earth.set_velocity(np.array([0,-1], dtype=float), EARTH_VELOCITY)
-
-#The spaceship
-#Spaceship = phys.Spaceship(900,900,96570,0,0,92670,340,2960,0,np.array([1, 0], dtype=float))
-Spaceship = phys.Spaceship(SPACESHIP_POS[0],SPACESHIP_POS[1],96570,0,0,92670,340*1000,2960,0,np.array([0, 1], dtype=float))
 
 
 Simulation = phys.Simulation()
-Simulation.list_of_objects["Earth"] = Earth
-Simulation.list_of_objects["Sun"] = Sun
-Simulation.list_of_objects["Spaceship"] = Spaceship
-
 display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
 display.fill((0, 0, 0))
 
 space_factor = 150*(10**7) #Since SI unit of distance is m we would have planets really far away in space, not visible on screen
 
 
+#The spaceship
+Spaceship = phys.Spaceship(SPACESHIP_POS[0],SPACESHIP_POS[1],96570,0,0,92670,340*1000,2960,0,np.array([0, 1], dtype=float))
+#Planetary bodies
+Sun = phys.MyPhysObject(0, 0, 1.9891*(10**30), 0, 0)
 Simulation.list_of_objects = data_laoder.load_data()
 Simulation.list_of_objects["Sun"] = Sun
 Simulation.list_of_objects["Spaceship"] = Spaceship
@@ -64,7 +55,6 @@ def draw_body(display, body, color, r):
     screen_pos = sim_position_to_screen_position(body.position)
     pygame.draw.circle(display, color,
                        center=(screen_pos[0], screen_pos[1]), radius=r, width=0)
-    #print(str(body.position[0]//space_factor+DISPLAY_WIDTH//2)+", "+str(body.position[1]//space_factor+DISPLAY_HEIGHT//2))
 
 def draw_spaceship(display, ship):
     pos = ship.position
